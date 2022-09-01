38efca2f97e74f5bc185ccbf6bad8e5bc4f96997
TC <dche211@mywhitecliffe.com>
2022-09-01 21:51:34 +1200

    Setting up bottom tab on Main.js

--- screens/Main.js
+++ screens/Main.js
@@ -1,6 +1,11 @@
 import { Provider as PaperProvider } from 'react-native-paper';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import groceries from '../screens/categories/groceries'
+import healthCare from '../screens/categories/healthCare'
+import other from '../screens/categories/other'
+import takeout from '../screens/categories/takeout'
+import transport from '../screens/categories/transport'
 
 
 const Tab = createMaterialBottomTabNavigator();
@@ -9,9 +14,11 @@ export default function Main() {
     <PaperProvider>
     <NavigationContainer>
       <Tab.Navigator initialRouteName='' screenOptions={{headerTitleAlign:'center'}} shifting={true}>
-      <Tab.Screen name="Groceries" component={WelcomeScreen} />
-        <Tab.Screen name="Transport" component={LoginScreen} />
-        <Tab.Screen name="Others" component={SettingsScreen} />
+      <Tab.Screen name="Groceries" component={groceries} />
+        <Tab.Screen name="Transport" component={transport} />
+        <Tab.Screen name="Health Care" component={healthCare} />
+        <Tab.Screen name="Takeout" component={takeout} />
+        <Tab.Screen name="Others" component={other} />
       </Tab.Navigator>
     </NavigationContainer>
     </PaperProvider>
