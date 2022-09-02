0f471aa4351fff5e84784abdbb59693e221c33a2
Tc <dche211@mywhitecliffe.com>
2022-09-02 14:12:33 +1200

    Added Register button

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,15 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Screen from '../components/Screen';
 import AppText from '../components/AppText';
+import AppButton from '../components/AppButton';
 
 export default function LoginScreen() {
   return (
     <Screen>
       <AppText>LoginScreen</AppText>
+      <AppButton title={'Login'}></AppButton>
+      <AppButton title={'Register'}></AppButton>
     </Screen>
   )
 }
