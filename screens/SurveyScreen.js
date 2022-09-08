e446e8420d53b079720b05eb6effcc0d66f60e5e
TC <dche211@mywhitecliffe.com>
2022-09-08 15:05:16 +1200

    clear input field

--- screens/SurveyScreen.js
+++ screens/SurveyScreen.js
@@ -11,29 +11,29 @@ export default function SurveyScreen() {
     const [weeklyIncome, setIncome] = useState();
     const [weeklyExpense, setExpense] = useState();
     const [weeklySaving, setSaving] = useState();
-    let Budget = weeklyIncome-weeklyExpense-weeklySaving
+    const [budget, calcBudget] = useState();
+    
     return (
     <KeyboardAvoidingView style={{flex:1}}>
       <ScrollView style={{marginTop:'1%'}}>
         <AppText>What is your weekly income?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklyIncome} onChangeText={(weeklyIncome)=> setIncome(weeklyIncome)}/>
-        <AppButton title={'Add'} onPress={()=>{Alert.alert("Value added")}}/>
-        <AppButton title={'Clear'} onPress={()=>{Alert.alert("Field cleared")}}/>
+        <AppButton title={'Add'} onPress={()=>{;Alert.alert("Value added")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setIncome("");Alert.alert("Field cleared")}}/>
 
         <AppText>What are your weekly expenses?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklyExpense} onChangeText={(weeklyExpense)=> setExpense(weeklyExpense)}/>
-        <AppButton title={'Add'} onPress={()=>{Alert.alert("Value added")}}/>
-        <AppButton title={'Clear'} onPress={()=>{Alert.alert("Field cleared")}}/>
+        <AppButton title={'Add'} onPress={()=>{;Alert.alert("Value added")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setExpense("");Alert.alert("Field cleared")}}/>
 
         <AppText>How much would you like to save in a week?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklySaving} onChangeText={(weeklySaving)=> setSaving(weeklySaving)}/>
-        <AppButton title={'Add'} onPress={()=>{Alert.alert("Value added")}}/>
-        <AppButton title={'Clear'} onPress={()=>{Alert.alert("Field cleared")}}/>
+        <AppButton title={'Add'} onPress={()=>{;Alert.alert("Value added")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setSaving("");Alert.alert("Field cleared")}}/>
 
         <AppText>The amount you have to spend this week is:</AppText>
-        <AppTextInput editable={false} placeholder={'$'} value={Budget}/>
-        <AppButton title={'Add'} onPress={()=>{Alert.alert("Value added")}}/>
-        <AppButton title={'Clear'} onPress={()=>{Alert.alert("Field cleared")}}/>
+        <AppTextInput editable={false} placeholder={'$'} value={``}/>
+
         <NextButton title={'Next'} onPress={()=>newPage.navigate("Main Page")}/>
       </ScrollView>
     </KeyboardAvoidingView>
