69d9b14a66889ee4e82e6915f1e8f76c4b84ef2d
TC <dche211@mywhitecliffe.com>
2022-09-10 20:05:02 +1200

    inputting hardcoded data into flatlist

--- screens/categories/groceries.js
+++ screens/categories/groceries.js
@@ -1,13 +1,27 @@
 import { FlatList, StyleSheet, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import AppButton from '../../components/AppButton';
+import AppText from '../../components/AppText';
+
+const data = [
+  {
+    title : "Gum",
+    cost : "$3",
+  }
+];
+const Item = ({item}) => (
+  <SafeAreaView style={{flexDirection:'row'}}>
+  <AppText>{item.title}</AppText>
+  <AppText style={{marginLeft:'10%'}}>{item.cost}</AppText>
+  </SafeAreaView>
+);
 
 export default function Groceries() {
   return (
     <SafeAreaView>
-      <Text></Text>
+      <AppText>$</AppText>
       <AppButton title={'Add'}/>
-        <FlatList style={styles.flatlist} ListEmptyComponent={ <Text style={styles.text}>There is nothing in this list</Text> }></FlatList>
+        <FlatList data={data} renderItem={Item} style={styles.flatlist} ListEmptyComponent={ <Text style={styles.text}>There is nothing in this list</Text> }></FlatList>
     </SafeAreaView>
   )
 }
