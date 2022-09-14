9dba2470736428a956b9ae96c19ff7f8b5e8697b
TC <dche211@mywhitecliffe.com>
2022-09-15 09:12:24 +1200

    Added comment to create_context_file

--- screens/create_context_file.js
+++ screens/create_context_file.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const BudgetContext = createContext();
+export const BudgetContext = createContext(); //This allows other files to use the context. The param needed to be passed can be done with useContext.
