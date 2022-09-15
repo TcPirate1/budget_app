18c7450ac09cf1ea78554f69c9f5faf932c55b39
TC <dche211@mywhitecliffe.com>
2022-09-16 00:27:07 +1200

    Style list and remove hardcoded data

--- screens/categories/Groceries.js
+++ screens/categories/Groceries.js
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text, SafeAreaView, Platform, Dimensions } from 'react-native';
+import { FlatList, StyleSheet, Text, SafeAreaView, Platform } from 'react-native';
 import React, { useContext } from 'react';
 import AppButton from '../../components/AppButton';
 import AppText from '../../components/AppText';
@@ -13,40 +13,11 @@ const data = [
     cost : "$3",
     date : today,
   },
-  {
-    title : "Gum",
-    cost : "$3",
-    date : today,
-  },
-  {
-    title : "Gum",
-    cost : "$3",
-    date : today,
-  },
-  {
-    title : "Gum",
-    cost : "$3",
-    date : today,
-  },
-  {
-    title : "Gum",
-    cost : "$3",
-    date : today,
-  },
-  {
-    title : "Gum",
-    cost : "$3",
-    date : today,
-  },
-  {
-    title : "Gum",
-    cost : "$3",
-    date : today,
-  },
 ];
 const Item = ({item}) => (
-  <SafeAreaView>
-  <Text style={styles.flatlistText}>{item.title}          {item.cost}          {item.date}</Text>
+  <SafeAreaView >
+  <AppText style={styles.flatlistText}>{item.title}           {item.cost}</AppText>
+  <AppText>{item.date}</AppText>
   </SafeAreaView>
 );
 
@@ -57,7 +28,7 @@ export default function Groceries() {
       <AppText style={{marginTop: Platform.OS === "android" ?'3%': 0}}>${budget}</AppText>
       <AppButton title={'Add'}/>
       <AppButton title={'Remove'}/>
-        <FlatList data={data} renderItem={Item} ListEmptyComponent={ <Text style={styles.text}>There is nothing in this list</Text> } ></FlatList>
+        <FlatList data={data} renderItem={Item} ListEmptyComponent={ <Text style={styles.emptylistText}>There is nothing in this list</Text> } ></FlatList>
     </SafeAreaView>
   )
 }
@@ -67,9 +38,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    width: '100%',
   },
-  text: {
+  emptylistText: {
     paddingTop: '3%',
     fontStyle: 'italic',
     textDecorationLine: 'underline',
@@ -78,7 +48,7 @@ const styles = StyleSheet.create({
   },
   flatlistText: {
     margin: '5%',
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'red',
   },
 })
