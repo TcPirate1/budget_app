1d248ade7323841f455420fe21d2e52d9d5eb099
Tc <dche211@mywhitecliffe.com>
2022-09-15 17:01:31 +1200

    Styling flatlist

--- screens/categories/Groceries.js
+++ screens/categories/Groceries.js
@@ -1,39 +1,84 @@
-import { FlatList, StyleSheet, Text, SafeAreaView } from 'react-native';
+import { FlatList, StyleSheet, Text, SafeAreaView, Platform, Dimensions } from 'react-native';
 import React, { useContext } from 'react';
 import AppButton from '../../components/AppButton';
 import AppText from '../../components/AppText';
 import { BudgetContext } from '../create_context_file';
 
+const date = new Date();
+const today = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+
 const data = [
   {
     title : "Gum",
     cost : "$3",
-  }
+    date : today,
+  },
+  {
+    title : "Gum",
+    cost : "$3",
+    date : today,
+  },
+  {
+    title : "Gum",
+    cost : "$3",
+    date : today,
+  },
+  {
+    title : "Gum",
+    cost : "$3",
+    date : today,
+  },
+  {
+    title : "Gum",
+    cost : "$3",
+    date : today,
+  },
+  {
+    title : "Gum",
+    cost : "$3",
+    date : today,
+  },
+  {
+    title : "Gum",
+    cost : "$3",
+    date : today,
+  },
 ];
 const Item = ({item}) => (
-  <SafeAreaView style={{flexDirection:'row'}}>
-  <AppText>{item.title}</AppText>
-  <AppText style={{marginLeft:'10%'}}>{item.cost}</AppText>
+  <SafeAreaView>
+  <Text style={styles.flatlistText}>{item.title}          {item.cost}          {item.date}</Text>
   </SafeAreaView>
 );
 
 export default function Groceries() {
   const budget = useContext(BudgetContext);
   return (
-    <SafeAreaView>
-      <AppText>${budget}</AppText>
+    <SafeAreaView style={styles.flatlist}>
+      <AppText style={{marginTop: Platform.OS === "android" ?'3%': 0}}>${budget}</AppText>
       <AppButton title={'Add'}/>
-        <FlatList data={data} renderItem={Item} style={styles.flatlist} ListEmptyComponent={ <Text style={styles.text}>There is nothing in this list</Text> }></FlatList>
+      <AppButton title={'Remove'}/>
+        <FlatList data={data} renderItem={Item} ListEmptyComponent={ <Text style={styles.text}>There is nothing in this list</Text> } ></FlatList>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   flatlist: {
-    alignSelf:'center',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
   text: {
+    paddingTop: '3%',
     fontStyle: 'italic',
-    textDecorationLine: 'underline'
+    textDecorationLine: 'underline',
+    fontSize: 20,
+    alignSelf: 'center',
+  },
+  flatlistText: {
+    margin: '5%',
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    fontSize: 18,
   },
 })
