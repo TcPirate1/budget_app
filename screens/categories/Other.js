61f9cab09c811a7d4cda0f7f3545e760280b5d17
Tc <dche211@mywhitecliffe.com>
2022-09-15 14:54:52 +1200

    Import useContext for missing screens

--- screens/categories/Other.js
+++ screens/categories/Other.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, {useContext} from 'react';
 import { BudgetContext } from '../create_context_file';
 
 export default function Other() {
