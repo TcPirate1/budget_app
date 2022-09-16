e6d8f43ba7bcb9978e89442b9274c9ceca59d136
Tc <dche211@mywhitecliffe.com>
2022-09-16 16:32:53 +1200

    Setup modal on Groceries.js

--- screens/categories/Groceries.js
+++ screens/categories/Groceries.js
@@ -1,7 +1,9 @@
 import { FlatList, Text, SafeAreaView, Platform } from 'react-native';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import Modal from "react-native-modal";
 import AppButton from '../../components/AppButton';
 import AppText from '../../components/AppText';
+import AppTextInput from '../../components/AppTextInput';
 import { BudgetContext } from '../create_context_file';
 import { Item } from '../Items';
 import defaultStyles from "../../config/styles";
@@ -17,13 +19,29 @@ const data = [
 
 export default function Groceries() {
   const budget = useContext(BudgetContext);
+
+  const [ModalVisibility, setModalVisibility] = useState(false);
+
+  const toggleModal = () =>{
+    setModalVisibility(!ModalVisibility);
+  };
+
   return (
     <SafeAreaView style={defaultStyles.flatlist}>
       <AppText style={{marginTop: Platform.OS === "android" ?'3%': 0}}>${budget}</AppText>
-      <AppButton title={'Add'}/>
-      <AppButton title={'Remove'}/>
+      <AppButton title='Add new item' onPress={toggleModal}/>
+      <AppButton title='Remove item'/>
         <FlatList data={data} renderItem={Item}
         ListEmptyComponent={ <Text style={defaultStyles.emptylistText}>There is nothing in this list</Text> } ></FlatList>
+
+        <Modal isVisible={ModalVisibility}>
+          <SafeAreaView style={{flex:1}}>
+            <AppText>Add your items here</AppText>
+            <AppTextInput></AppTextInput>
+            <AppButton title='Hide Modal' onPress={toggleModal}/>
+            <AppButton title='Add' onPress={toggleModal}/>
+          </SafeAreaView>
+        </Modal>
     </SafeAreaView>
   )
 }
