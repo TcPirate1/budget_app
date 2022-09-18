f99b7d51caf8b77ff977f3038f930acda288f05b
TC <dche211@mywhitecliffe.com>
2022-09-19 01:14:51 +1200

    Copy and pasted Groceries page layout to other category pages

--- screens/categories/HealthCare.js
+++ screens/categories/HealthCare.js
@@ -1,13 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, {useContext} from 'react';
+import { FlatList, Text, SafeAreaView, Platform } from 'react-native';
+import React, { useContext, useState } from 'react';
+import Modal from "react-native-modal";
+import AppButton from '../../components/AppButton';
+import AppText from '../../components/AppText';
+import AppTextInput from '../../components/AppTextInput';
 import { BudgetContext } from '../create_context_file';
+import { Item } from '../Items';
+import defaultStyles from "../../config/styles";
+import { today } from '../Date';
 
 export default function HealthCare() {
   const budget = useContext(BudgetContext);
+
+  const [ModalVisibility, setModalVisibility] = useState(false);
+
+  const toggleModal = () =>{
+    setModalVisibility(!ModalVisibility);
+  };
+
+  const [productName, setProductName] = useState();
+  const [price, setPrice] = useState(); //These are changed by the text input from modal
+  
+  const [initialElements, changeEl] = useState([]);
+  const [exampleState, setExampleState] = useState(initialElements);
+  const [idx, incr] = useState(initialElements.length);
+
+  const addElement = () => {
+    let newArray = [...initialElements , {id: idx, product: `${productName}`, cost : `$${price}`, date : today}];
+    incr(idx + 1);
+    setExampleState(newArray);
+    changeEl(newArray);
+  }
+
   return (
-    <View>
-      <Text>healthCare</Text>
-    </View>
+    <SafeAreaView style={defaultStyles.flatlist}>
+      <AppText style={{marginTop: Platform.OS === "android" ?'3%': 0}}>${budget}</AppText>
+      <AppButton title='Add new item' onPress={toggleModal}/>
+        <FlatList data={exampleState} renderItem={Item} keyExtractor={item=>item.id} ListEmptyComponent={ <Text style={defaultStyles.emptylistText}>There is nothing in this list</Text> } />
+
+        <Modal isVisible={ModalVisibility} transparent={false}>
+          <SafeAreaView style={{flex:1, alignItems:'center', justifyContent:'center' }}>
+            <AppText style={{backgroundColor:'green', color:'white', borderRadius:15, padding:'3%'}}>Add your items here</AppText>
+            <AppTextInput placeholder='Product name' onChangeText={(productName)=>setProductName(productName)}/>
+            <AppTextInput placeholder='Cost' onChangeText={(price)=>setPrice(price)}/>
+            <AppButton title='Add' onPress={()=>{addElement();toggleModal()}}/>
+            <AppButton title='Cancel' onPress={toggleModal}/>
+          </SafeAreaView>
+        </Modal>
+    </SafeAreaView>
   )
 }
 
