150ec96327a4c3f7917f543cc3cfbda20983f874
TC <dche211@mywhitecliffe.com>
2022-09-18 23:29:18 +1200

    Fixed warning. Changed 0 to undefined when setting states

--- screens/SurveyScreen.js
+++ screens/SurveyScreen.js
@@ -19,21 +19,22 @@ export default function SurveyScreen() {
     else{
       budget = budget;
     }
-    //Try removing the placeholders for the textinput (there is a warning that textinput is expecting a string when clear buttons are pressed)
+
+    //Using undefined because states initially are. Lets placeholder be displayed when values are cleared.
     return (
     <KeyboardAvoidingView style={{flex:1}}>
       <ScrollView style={{marginTop:'1%'}}>
         <AppText>What is your weekly income?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklyIncome} onChangeText={(weeklyIncome) => setIncome(weeklyIncome)}/>
-        <AppButton title={'Clear'} onPress={()=>{setIncome(0);Alert.alert("Field cleared")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setIncome(undefined);Alert.alert("Field cleared")}}/>
 
         <AppText>What are your weekly expenses?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklyExpense} onChangeText={(weeklyExpense) => setExpense(weeklyExpense)}/>
-        <AppButton title={'Clear'} onPress={()=>{setExpense(0);Alert.alert("Field cleared")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setExpense(undefined);Alert.alert("Field cleared")}}/>
 
         <AppText>How much would you like to save in a week?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklySaving} onChangeText={(weeklySaving) => setSaving(weeklySaving)}/>
-        <AppButton title={'Clear'} onPress={()=>{setSaving(0);Alert.alert("Field cleared")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setSaving(undefined);Alert.alert("Field cleared")}}/>
 
         <AppText>The amount you have to spend this week is:</AppText>
         <AppTextInput editable={false}>${budget}</AppTextInput>
