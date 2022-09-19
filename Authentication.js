1e0e8a3bbbcfb266a9bc112f80a3f846f7a506c8
TC <dche211@mywhitecliffe.com>
2022-09-19 16:35:39 +1200

    implement firebase into project

--- Authentication.js
+++ Authentication.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps} from "firebase/app";
-import { getAuth} from "firebase/auth";
+import * as firebase from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,12 +15,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 let app;
-if (getApps().length === 0) {
-    app = initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig);
 } else {
-    app = getApp();
+    app = firebase.app()
 }
 
-const auth = getAuth(app);
+const auth = firebase.auth()
 
 export { auth };
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,12 +5,16 @@ import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
 import LoginButton from '../components/LoginButton';
 import { useNavigation } from '@react-navigation/native';
+import { auth } from '../Authentication';
 
 export default function LoginScreen() {
   const newPage = useNavigation();
 
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+
+  const handleSignup = () => {}
+  
   return (
     <Screen>
       <AppText>Login with your email</AppText>
