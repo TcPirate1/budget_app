5d26f05035bade1f60c02fc7657e32a7809931f2
TC <dche211@mywhitecliffe.com>
2022-09-22 20:04:27 +1200

    Last textinput now updates dynamically

--- screens/SurveyScreen.js
+++ screens/SurveyScreen.js
@@ -26,15 +26,15 @@ export default function SurveyScreen() {
       <ScrollView style={{marginTop:'1%'}}>
         <AppText>What is your weekly income?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklyIncome} onChangeText={(weeklyIncome) => setIncome(weeklyIncome)}/>
-        <AppButton title={'Clear'} onPress={()=>{setIncome(undefined);Alert.alert("Field cleared")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setIncome('');Alert.alert("Field cleared")}}/>
 
         <AppText>What are your weekly expenses?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklyExpense} onChangeText={(weeklyExpense) => setExpense(weeklyExpense)}/>
-        <AppButton title={'Clear'} onPress={()=>{setExpense(undefined);Alert.alert("Field cleared")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setExpense('');Alert.alert("Field cleared")}}/>
 
         <AppText>How much would you like to save in a week?</AppText>
         <AppTextInput keyboardType="numeric" placeholder={'$'} value={weeklySaving} onChangeText={(weeklySaving) => setSaving(weeklySaving)}/>
-        <AppButton title={'Clear'} onPress={()=>{setSaving(undefined);Alert.alert("Field cleared")}}/>
+        <AppButton title={'Clear'} onPress={()=>{setSaving('');Alert.alert("Field cleared")}}/>
 
         <AppText>The amount you have to spend this week is:</AppText>
         <AppTextInput editable={false}>${weeklyBudget}</AppTextInput>
