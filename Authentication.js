b8d6a5903fc5eece65e2a5b5bc63d2350440db93
TC <dche211@mywhitecliffe.com>
2022-09-30 19:58:23 +1300

    Edited comments and using initializeAuth to be able to use AsyncStorage from react-native-async-storage

--- Authentication.js
+++ Authentication.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { getAuth } from "firebase/auth"; This still uses the depreciated asyncstorage dependency from react-native
+import { initializeAuth, getReactNativePersistence } from "firebase/auth/react-native";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBvc4t8jFRDuBp96dzuWmI9ZLaXbTkc6pA",
   authDomain: "budgetapp-3c941.firebaseapp.com",
@@ -16,4 +14,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+//Initialize Authentication (Allows more control, as seen in initializeAuth doc, than getAuth and allows us to use the not depreciated AsyncStorage)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+export { auth };
