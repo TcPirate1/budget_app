815038d496a45ac4b91ffe2c88c8f1feba56ca32
TC <dche211@mywhitecliffe.com>
2022-10-05 14:21:39 +1300

    Attempt 2, hopefully no merge conflicts

--- screens/BudgetContext.js
+++ screens/BudgetContext.js
@@ -1,4 +1,3 @@
 import { createContext } from "react";
-import { weeklyBudget } from "./SurveyScreen";
 
-export const BudgetContext = createContext(weeklyBudget); //This allows other files to use the context. The param needed to be passed can be done with useContext.
+export const BudgetContext = createContext(); //This allows other files to use the context. The param needed to be passed can be done with useContext.
